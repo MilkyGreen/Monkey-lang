fab7ed417a305ddc82cdb9751995368531dfe1b3
李云梦 <yunmeng.li@successchannel.com>
2022-09-07 10:57:13 +0800

    eval

--- repl/repl.go
+++ repl/repl.go
@@ -4,12 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
 
 const PROMPT = ">> "
-
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -27,8 +27,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
